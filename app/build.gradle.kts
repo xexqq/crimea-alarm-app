79c197393cee6ab4dcf932448595d8e04eca7f70
xexqq <_@example.com>
2026-09-08 18:46:21 +0300

    Убраны дублирующиеся плагины

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,6 @@
 plugins {
-    id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
     id("com.google.devtools.ksp") version "1.9.20-1.0.14"
 }
 
